a1762926922c965953d6bc0143ba948490966308
javiregistros71@gmail.com <javiregistros71@gmail.com>
2023-08-08 16:46:50 +0900

    Working pinging app

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList, StyleSheet, Modal} from 'react-native';
+import { View, Text, TextInput, Button, FlatList, StyleSheet, Modal, ToastAndroid} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { NavigationContainer, useNavigation, useIsFocused } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 const Stack = createNativeStackNavigator();
+
+const showToast = (text) => {
+  ToastAndroid.show(text, ToastAndroid.SHORT);
+};
+
+const getUniqueID = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -18,6 +27,9 @@ const HomeScreen = () => {
         if (peopleData) {
           setPeople(JSON.parse(peopleData));
         }
+        else{
+          showToast("Good job, your network is healthy!");
+        }
       } catch (error) {
         console.log('Error loading people:', error);
       }
@@ -26,6 +38,22 @@ const HomeScreen = () => {
     loadPeople();
   }, []);
 
+  const reloadPeople = async () => {
+    try {
+      const peopleData = await AsyncStorage.getItem('people');
+      if (peopleData) {
+        setPeople(JSON.parse(peopleData));
+      }
+    } catch (error) {
+      console.log('Error loading people:', error);
+    }
+  };
+
+  useEffect(() => {
+    reloadPeople();
+  }, []);
+
+  
   const handleContacted = (index) => {
     const updatedPeople = [...people];
     const now = Date.now();
@@ -34,7 +62,7 @@ const HomeScreen = () => {
     AsyncStorage.setItem('people', JSON.stringify(updatedPeople))
       .catch(error => {
         console.log('Error saving people:', error);
-      });
+      }); 
   };
 
   const renderItem = ({ item, index }) => {
@@ -47,7 +75,7 @@ const HomeScreen = () => {
       );
     }
     return null;
-  };
+  }; 
 
   const shouldAppear = (lastContacted, period, periodUnit) => {
     const timeSinceContacted = Date.now() - lastContacted;
@@ -77,10 +105,10 @@ const HomeScreen = () => {
         contentContainerStyle={styles.listContainer}
         ListEmptyComponent={<Text style={styles.emptyText}>You are up to date!</Text>}
       />
-      <View style={styles.buttonContainer}>
+      <View style={styles.buttonContainerMain}>
         <Button
           title="Add Person"
-          onPress={() => navigation.navigate('List')}
+          onPress={() => navigation.navigate('List')} color='black' 
         />
       </View>
     </View>
@@ -90,25 +118,40 @@ const HomeScreen = () => {
 const ListScreen = () => {
   const [people, setPeople] = useState([]);
   const [name, setName] = useState('');
-  const [period, setPeriod] = useState('');
-  const [periodUnit, setPeriodUnit] = useState('minutes');
-  const [country, setCountry] = useState('');
-  const [selectedCountry, setSelectedCountry] = useState('');
+  const [period, setPeriod] = useState(''); 
+  const [periodUnit, setPeriodUnit] = useState('days');
+  const [country, setCountry] = useState('Spain');
+  const [selectedCountry, setSelectedCountry] = useState('Spain'); 
   const [modalVisible, setModalVisible] = useState(false);
   const [newCountry, setNewCountry] = useState('');
   const [countryOptions, setCountryOptions] = useState(['Spain', 'Korea']);
 
   useEffect(() => {
+    const loadCountryOptions = async () => {
+      try {
+        const savedCountryOptions = await AsyncStorage.getItem('countryOptions');
+        if (savedCountryOptions) {
+          setCountryOptions(JSON.parse(savedCountryOptions));
+        }
+      } catch (error) {
+        console.log('Error loading country options:', error);
+      }
+    };
+  
+    loadCountryOptions();
+  }, []);
+
+  useEffect(() => {
     const loadPeople = async () => {
       try {
-        const peopleData = await AsyncStorage.getItem('people');
+        const peopleData = await AsyncStorage.getItem('people'); 
         if (peopleData) {
           setPeople(JSON.parse(peopleData));
         }
       } catch (error) {
         console.log('Error loading people:', error);
       }
-    };
+    };   
 
     loadPeople();
   }, []);
@@ -128,6 +171,7 @@ const ListScreen = () => {
   const addPerson = async () => {
     if (name && period && periodUnit && country) {
       const newPerson = {
+        id: getUniqueID(), // Add a unique ID
         name,
         period,
         periodUnit,
@@ -139,7 +183,7 @@ const ListScreen = () => {
       setPeople(updatedPeople);
       setName('');
       setPeriod('');
-      setPeriodUnit('minutes');
+      setPeriodUnit('');
       setCountry('');
       try {
         await AsyncStorage.setItem('people', JSON.stringify(updatedPeople));
@@ -159,14 +203,16 @@ const ListScreen = () => {
     });
   };
 
-  const deletePerson = (index) => {
-    const updatedPeople = [...people];
-    updatedPeople.splice(index, 1);
+  const deletePerson = (id) => {
+    const updatedPeople = people.filter(person => person.id !== id);
     setPeople(updatedPeople);
-    AsyncStorage.setItem('people', JSON.stringify(updatedPeople)).catch(error => {
-      console.log('Error saving people:', error);
-    });
+    AsyncStorage.setItem('people', JSON.stringify(updatedPeople))
+      .catch(error => {
+        console.log('Error saving people:', error);
+      });
   };
+  
+  
 
   const filterPeople = () => {
     if (selectedCountry === '') {
@@ -237,14 +283,14 @@ const ListScreen = () => {
       />
       <Picker
         selectedValue={item.periodUnit}
-        style={styles.picker}
+        style={styles.picker2}
         onValueChange={value => editPerson(index, item.period, value)}
       >
-        <Picker.Item label="Minutes" value="minutes" />
-        <Picker.Item label="Hours" value="hours" />
         <Picker.Item label="Days" value="days" />
+        <Picker.Item label="Hours" value="hours" /> 
+        <Picker.Item label="Minutes" value="minutes" />        
       </Picker>
-      <Button title="Delete" onPress={() => deletePerson(index)} />
+      <Button title="Delete" onPress={() => deletePerson(item.id)} color='black'/>
     </View>
   );
 
@@ -266,12 +312,13 @@ const ListScreen = () => {
         />
         <Picker
           selectedValue={periodUnit}
-          style={styles.picker}
+          style={styles.picker0}
           onValueChange={value => setPeriodUnit(value)}
         >
-          <Picker.Item label="Minutes" value="minutes" />
-          <Picker.Item label="Hours" value="hours" />
           <Picker.Item label="Days" value="days" />
+          <Picker.Item label="Hours" value="hours" />
+          <Picker.Item label="Minutes" value="minutes" />
+                    
         </Picker>
         <Picker
           selectedValue={country}
@@ -281,7 +328,7 @@ const ListScreen = () => {
           {renderCountryOptions()}
           <Picker.Item label="Add New Country" value="Add New Country" />
         </Picker>
-        <Button title="Add Person" onPress={addPerson} />
+        <Button title="Add Person" onPress={addPerson} color='black' />
       </View>
       <View style={styles.filterContainer}>
         <Picker
@@ -295,19 +342,20 @@ const ListScreen = () => {
       <FlatList
         data={filterPeople()}
         renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
+        keyExtractor={(item) => item.id.toString()} // Use the unique ID as the key
         contentContainerStyle={styles.listContainer}
         ListEmptyComponent={<Text>No people found</Text>}
       />
-      <Button
-        title="Save Changes"
+      <View style={styles.buttonContainerMain}>
+      <Button title="Save Changes"
         onPress={() => {
           AsyncStorage.setItem('people', JSON.stringify(people))
             .catch(error => {
               console.log('Error saving people:', error);
             });
-        }}
-      />
+        }} color='black'/>
+      </View>
+
       <Modal visible={modalVisible} animationType="slide">
         <View style={styles.modalContainer}>
           <TextInput
@@ -316,9 +364,9 @@ const ListScreen = () => {
             placeholder="New Country"
             onChangeText={text => setNewCountry(text)}
           />
-          <Button title="Add" onPress={handleAddCountry} />
-          <Button title="Remove" onPress={handleRemoveCountry} />
-          <Button title="Cancel" onPress={() => setModalVisible(false)} />
+          <Button title="Add" onPress={handleAddCountry} color='black' />
+          <Button title="Remove" onPress={handleRemoveCountry} color='black' />
+          <Button title="Cancel" onPress={() => setModalVisible(false)} color='black' />
         </View>
       </Modal>
     </View>
@@ -350,7 +398,7 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: 30,
     paddingHorizontal: 16,
   },
   addContainer: {
@@ -363,8 +411,17 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
   },
+  picker0: {
+    height: 40,
+    marginBottom: 0,
+  },
   picker: {
     height: 40,
+    marginBottom: 10,
+  },
+  picker2: {
+    height: 40,
+    width: 120,
     marginBottom: 10,
   },
   listContainer: {
@@ -384,6 +441,10 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 30,
+  },
+  buttonContainerMain: {
+    marginTop: 30,
+    marginBottom:25,
   },
 });
 
